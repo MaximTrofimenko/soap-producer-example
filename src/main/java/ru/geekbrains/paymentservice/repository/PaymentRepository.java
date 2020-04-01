448d85dd4eaa8891e2ad6e7c06b594605af4cd7c
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-01 18:59:04 +0300

    добавил fee в репозиторий

--- src/main/java/ru/geekbrains/paymentservice/repository/PaymentRepository.java
+++ src/main/java/ru/geekbrains/paymentservice/repository/PaymentRepository.java
@@ -23,21 +23,25 @@ public class PaymentRepository {
         payment1.setId(1);
         payment1.setName("PayPal");
         payment1.setCountry("USA");
+        payment1.setFee(120);
 
         Payment payment2 = new Payment();
         payment2.setId(2);
         payment2.setName("GooglePay");
         payment2.setCountry("USA");
+        payment1.setFee(130);
 
         Payment payment3 = new Payment();
         payment3.setId(3);
         payment3.setName("WebMoney");
         payment3.setCountry("Russia");
+        payment1.setFee(600);
 
         Payment payment4 = new Payment();
         payment4.setId(4);
         payment4.setName("YandexMoney");
         payment4.setCountry("Russia");
+        payment1.setFee(800);
 
         return new ArrayList<Payment>() {{
             add(payment1);
